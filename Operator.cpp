e7596d3f52fa10da683b69e5400f85f1be2db2c4
Anirudh Jain <500076106@stu.upes.ac.in>
2021-12-05 18:19:10 +0530

    Added Classes and implemented concepts of Object Oriented Programming
    Co-authored-by: Love-Sharma2509 <Love-Sharma2509@users.noreply.github.com>

--- Operator.cpp
+++ Operator.cpp
@@ -4,18 +4,57 @@
 
 using namespace std;
 
-int sobx[3][3] = { {-1,0,1},
-					{-2,0,2},
-					{-1,0,1} };
-int soby[3][3] = { {1,2,1},
-					{0,0,0},
-					{-1,-2,-1} };
-int prex[3][3]={{1,0,-1},
-				{1,0,-1},
-				{1,0,-1}};
-int prey[3][3]={{1,1,1},
-				{0,0,0},
-				{-1,-1,-1}};
+class Mask
+{
+	public:
+	const int SOBX[3][3] = {{-1, 0, 1},
+							{-2, 0, 2},
+							{-1, 0, 1}};
+	const int SOBY[3][3] = {{1, 2, 1},
+							{0, 0, 0},
+							{-1, -2, -1}};
+	const int PREX[3][3] = {{1, 0, -1},
+							{1, 0, -1},
+							{1, 0, -1}};
+	const int PREY[3][3] = {{1, 1, 1},
+							{0, 0, 0},
+							{-1, -1, -1}};
+};
+
+class Calculation : public Mask
+{
+private:
+	const int mr = 1;
+	int maxVal = 0;
+
+public:
+	int gradient(double *x, double *y, double *mag, int height, int width)
+	{
+		int maxVal = 0;
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+			*((mag+i*width)+j) = sqrt((*((x+i*width)+j) * *((x+i*width)+j) + *((y+i*width)+j) * *((y+i*width)+j)));
+
+			if (*((mag+i*width)+j) > maxVal)
+				maxVal = *((mag+i*width)+j);
+			}
+		}
+		return maxVal;
+	}
+	void scaling(double *a, int max, int height, int width)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				(*((a + i * width) + j)) = (*((a + i * width) + j)) / max * 255;
+			}
+		}
+	}
+};
+
 int main(int argc, char **argv)
 {
 	if (argc != 4)
@@ -32,7 +71,7 @@ int main(int argc, char **argv)
 	{
 		cout << "File " << path << " not found in directory." << endl;
 		return 0;
-	}	
+	}
 
 	ofstream img_mag("./output_images/output_mag.pgm", ios::binary);
 	ofstream img_hi("./output_images/output_hi.pgm", ios::binary);
@@ -42,30 +81,53 @@ int main(int argc, char **argv)
 
 	char buffer[1024];
 	int width, height, intensity, hi = stoi(argv[2]), lo = stoi(argv[3]);
-	int sumx, sumy,num,arrx[3][3],arry[3][3];
-	cout << "Choose which function you want to perform:-"<<endl<<"Press 1 for Sobel Operator"<<endl<<"Press 2 for Prewitt Operator"<<endl;
+
+	int sumx, sumy, num, arrx[3][3], arry[3][3];
+	cout << "Choose which function you want to perform:-" << endl
+		 << "Press 1 for Sobel Operator" << endl
+		 << "Press 2 for Prewitt Operator" << endl;
 	cin >> num;
-	switch(num){
-		case 1: for(int i=0;i<3;i++){
-			for(int j=0;j<3;j++){
-				arrx[i][j]=sobx[i][j];
-				arry[i][j]=soby[i][j];
+
+	Calculation calculate;
+
+	switch (num)
+	{
+	case 1:
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				arrx[i][j] = calculate.SOBX[i][j];
+				arry[i][j] = calculate.SOBY[i][j];
 			}
 		}
-		case 2:for(int i=0;i<3;i++){
-			for(int j=0;j<3;j++){
-				arrx[i][j] = prex[i][j];
-				arry[i][j] = prey[i][j];
+	case 2:
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				arrx[i][j] = calculate.PREX[i][j];
+				arry[i][j] = calculate.PREY[i][j];
 			}
 		}
 	}
 	// Storing header information and copying into the new ouput images
-	infile  >> buffer >> width >> height >> intensity;
-	img_mag << buffer << endl << width << " " << height << endl << intensity << endl;
-	img_hi  << buffer << endl << width << " " << height << endl << intensity << endl;
-	img_lo  << buffer << endl << width << " " << height << endl << intensity << endl;
-	img_x   << buffer << endl << width << " " << height << endl << intensity << endl;
-	img_y   << buffer << endl << width << " " << height << endl << intensity << endl;
+	infile >> buffer >> width >> height >> intensity;
+	img_mag << buffer << endl
+			<< width << " " << height << endl
+			<< intensity << endl;
+	img_hi << buffer << endl
+		   << width << " " << height << endl
+		   << intensity << endl;
+	img_lo << buffer << endl
+		   << width << " " << height << endl
+		   << intensity << endl;
+	img_x << buffer << endl
+		  << width << " " << height << endl
+		  << intensity << endl;
+	img_y << buffer << endl
+		  << width << " " << height << endl
+		  << intensity << endl;
 
 	// These matrices will hold the integer values of the input image
 	double pic[height][width], x[height][width], y[height][width], mag[height][width];
@@ -95,8 +157,8 @@ int main(int argc, char **argv)
 					if ((i + p) < 0 || (j + q) < 0 || (i + p) >= height || (j + q) >= width)
 						continue;
 
-					sumx += pic[i+p][j+q] * arrx[p+mr][q+mr];
-					sumy += pic[i+p][j+q] * arry[p+mr][q+mr];
+					sumx += pic[i + p][j + q] * arrx[p + mr][q + mr];
+					sumy += pic[i + p][j + q] * arry[p + mr][q + mr];
 				}
 			}
 
@@ -110,33 +172,15 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// Make sure all the values are between 0-255
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			x[i][j] = x[i][j] / maxx * 255;
-			y[i][j] = y[i][j] / maxy * 255;
-		}
-	}
+	// Scaling
+	calculate.scaling(&x[0][0], maxx, height, width);
+	calculate.scaling(&y[0][0], maxy, height, width);
 
-	// Find gradient and maxval
-	int maxVal = 0;
-	for (int i = 0; i < height; i++)
-	{
-		for(int j = 0; j < width; j++)
-		{
-			mag[i][j] = sqrt((x[i][j] * x[i][j]) + (y[i][j] * y[i][j]));
+	// Gradient and MaxVal calculation
+	int maxVal = calculate.gradient(&x[0][0], &y[0][0], &mag[0][0], height, width);
 
-			if (mag[i][j] > maxVal)
-				maxVal = mag[i][j];
-		}
-	}
-
-	// Make sure all the magnitude values are between 0-255
-	for (int i = 0; i < height; i++)
-		for (int j = 0; j < width; j++)
-			mag[i][j] = mag[i][j] / maxVal * 255;
+	// Scaling final output image
+	calculate.scaling(&mag[0][0], maxVal, height, width);
 
 	// Make sure to cast back to char before outputting
 	// Also to avoid any wonky results, get rid of any decimals by casting to int first
@@ -153,13 +197,13 @@ int main(int argc, char **argv)
 			// Output the magnitude image
 			img_mag << (char)((int)mag[i][j]);
 
-			// Ouput the low threshold image
-			if (mag[i][j] > lo)
+			// Output the low threshold image
+			if (mag[i][j] < lo)
 				img_lo << (char)255;
 			else
 				img_lo << (char)0;
 
-			// Ouput the high threshold image
+			// Output the high threshold image
 			if (mag[i][j] > hi)
 				img_hi << (char)255;
 			else
